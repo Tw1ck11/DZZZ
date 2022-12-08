8e687a9237c8a7417be14ffd534be46772871235
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-12-08 13:07:56 +0200

    DZZZ

---
+++ DZZZ.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Device
+{
+	string type;
+	string material;
+	string name;
+	string company;
+	double weight;
+	double price;
+};
+
+Device fillTheDevice()
+{
+	Device device;
+	cout << "Enter device type: ";
+	cin >> device.type;
+	cout << "Enter material: ";
+	cin >> device.material;
+	cout << "Enter name: ";
+	cin >> device.name;
+	cout << "Enter device company: ";
+	cin >> device.company;
+	cout << "Enter price: ";
+	cin >> device.price;
+	cout << "Enter weight: ";
+	cin >> device.weight;
+
+	return device;
+}
+
+void ShowDevice(const Device& device)
+{
+	cout << "Type of device: " << device.type << "\n";
+	cout << "Material: " << device.material << "\n";
+	cout << "Name: " << device.name << "\n";
+	cout << "Device company: " << device.company << "\n";
+	cout << "Price: " << device.price << "\n";
+	cout << "Weight: " << device.weight << "\n";
+}
+
+int main()
+{
+	Device device;
+	device = fillTheDevice();
+
+	ShowDevice(device);
+}
